a744703a9dfd8d377c954666c60e5cd240d07d29
GuyMeuris <guymeuris@yahoo.com>
2021-01-08 09:45:34 +0100

    Add project files.

---
+++ Plantenhotel/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿/* Groepswerk WPF app: "Plantenhotel"
+ * Deelnemers: Nina Roland, Benjamin Donck, Nigel De Maeght, Guy Meuris
+ * Gemaakt in januari 2021
+
+   Deze app geeft een oplossing voor bedrijven die een plantenhotel uitbaten.
+*/
+
+using System.Windows;
+
+namespace Plantenhotel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
