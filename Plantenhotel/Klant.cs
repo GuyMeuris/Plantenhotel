db1f3e89c2d27b2931c8b890d3908c079a3ed126
GuyMeuris <guymeuris@yahoo.com>
2021-01-15 13:53:15 +0100

    Aanpassingen in de  Persoon Klant Medewerker klassen

--- Plantenhotel/Klant.cs
+++ Plantenhotel/Klant.cs
@@ -17,7 +17,6 @@ namespace Plantenhotel
         ) : Persoon( Achternaam, Voornaam, Geboortedatum, Gsmnr, Emailadres, Gebruikersnaam, Wachtwoord )
     {
         // velden
-        public static List<Klant> lijstKlanten = new List<Klant>();
         private int aantalBestellingen = 0;
         private bool isParticulier = true;
         private string BTWnr = "n.v.t.";
@@ -27,16 +26,6 @@ namespace Plantenhotel
         // methoden op instance-niveau
 
         // methoden op record-niveau
-        public static void ToonNamenAlleKlanten()
-        {
-            Console.Write( "Alle geregistreerde klanten:" );
-            for ( int i = 0; i <= lijstKlanten.Count - 1; i++ )
-            {
-                Console.Write( $"{(i == 0 ? " " : "")}{lijstKlanten[i].Naam}{(i == lijstKlanten.Count - 2 ? " en " : "")}" +
-                                $"{ (i < lijstKlanten.Count - 2 ? ", " : "")}" );
-            }
-            Console.WriteLine( "." );
-        }
 
     }
 
