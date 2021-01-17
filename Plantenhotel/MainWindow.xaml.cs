46e0326057553971a6897731c1c9be21f748155e
Nina <nina.roland@hotmail.com>
2021-01-17 16:45:04 +0100

    Guy, ik heb gezien dat ik in de master heb gewerkt. Ik ga het vanaf nu altijd eerst checken, heb niets bij jullie code veranderd :)

--- Plantenhotel/MainWindow.xaml.cs
+++ Plantenhotel/MainWindow.xaml.cs
@@ -19,9 +19,20 @@ namespace Plantenhotel
             InitializeComponent();
         }
 
-        private void Knop1_Click( object sender, RoutedEventArgs e )
-        {
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Home.Content = new Login();
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            Home.Content = new OfferteAanvraag();
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            Home.Content = new Registratie();
         }
     }
 }
