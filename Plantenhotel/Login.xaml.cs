83e037aba0ae31f5c0a443c5bc92b7066b90b6d2
Nigel <nigel.demaeght@gmail.com>
2021-01-26 12:23:30 +0100

    update login pagina

--- Plantenhotel/Login.xaml.cs
+++ Plantenhotel/Login.xaml.cs
@@ -16,11 +16,71 @@ using System.Windows.Shapes;
 
 namespace Plantenhotel
 {
+
     public partial class Login
     {
-       
+        public Login()
+        {
+            InitializeComponent();
+        }
+        List<string> gebruikers = new List<string>();
+        List<string> wachtwoorden = new List<string>();
+
+        private void loginKnop_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (tbGebruikersnaam.Text.Contains("DeSchuur") && tbWachtwoord.Text.Contains("Fotosynthese"))
+            {
+
+            }
+            else
+            {
+                //KanInloggen(_gebruiker, _wachtwoord);
+                StreamReader sr = new StreamReader(@"C:\Visual Studio Repos\GebruikersnaamWachtwoord.txt");
+                string components = sr.ReadToEnd();
+                //components splitsen naar array
+                String[] regels = components.Split("\n");
+                sr.Close();
+
+                foreach (var item in regels)
+                {
+                    string separator = " ";
+                    string item2 = item.Trim();
+                    int separatorIndex = item2.IndexOf(separator);
+                    if (separatorIndex > 0)
+                    {
+                        string gebruikersnaam = item2.Substring(0, separatorIndex);
+                        gebruikers.Add(gebruikersnaam);
+                        string wachtwoord = item2.Substring(separatorIndex + 1);
+                        wachtwoorden.Add(wachtwoord);
+                    }
+                }
+                string user = tbGebruikersnaam.Text.Trim();
+                string paswoord = tbWachtwoord.Text.Trim();
+                if (gebruikers.Contains(user) && wachtwoorden.Contains(paswoord)
+                    && gebruikers.IndexOf(user) == wachtwoorden.IndexOf(paswoord))
+                {
+                    MessageBox.Show("U bent ingelogd!");
+                    //naar Menu
+                }
+                else
+                    MessageBox.Show("Gebruikersnaam of wachtwoord is niet juist!");
+            }
+        }
 
     }
-
-
+ 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+           
