3b0018e7757710964f35b33af20bf0a9c9e03ba7
GuyMeuris <guymeuris@yahoo.com>
2021-01-27 23:06:30 +0100

    Aanpassing aan home-pagina

--- Plantenhotel/Home.xaml.cs
+++ Plantenhotel/Home.xaml.cs
@@ -13,34 +13,19 @@ namespace Plantenhotel
         public string Info { get; set; }
 
 
+
         public Home()
         {
             InitializeComponent();
             DataContext = this;
-
-            Info = @"Als u zich, net als wij, zorgen maakt over hoe
-uw kamerplanten zullen overleven terwijl u op 
-vakantie bent, vrees dan niet, want PlantenB&B 
-""De Schuur"" is hier om uw planten levend en 
-gezond te houden in een '5-sterren all-inclusive' 
-botanisch hotel.
-Online reserveren kan al vanaf 3 planten en wij 
-komen ze zelfs bij u thuis ophalen én terugbrengen!
-Als u bij ons boekt, worden uw planten verzorgd
-door een team van specialisten, zodat ze geliefd 
-en gehydrateerd blijven terwijl u geniet van een 
-onbezorgde vakantie zonder schuldgevoel. U wordt 
-zelfs uitgenodigd om de persoonlijkheidskenmerken
-en specifieke behoeften van uw planten te delen
-om ervoor te zorgen dat ze de allerbeste zorg 
-krijgen.";
         }
 
         private void Home_Load( object sender, RoutedEventArgs e )
         {
             naamBedrijf.Content = DisplayBedrijfsinfo( 0, ";" );
 
-            // infoBedrijf.Text = DisplayBedrijfsinfo( 1, "Bedrijfsinfo:" );
+            infoBedrijf.Text = DisplayBedrijfsinfo( 1, "Bedrijfsinfo:" );
+
         }
 
         private static string DisplayBedrijfsinfo( int i, string separator )
