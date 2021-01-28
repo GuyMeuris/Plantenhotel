41c3c9dc2ba2b3593315d401c7ffbe11a0077723
GuyMeuris <guymeuris@yahoo.com>
2021-01-28 21:37:51 +0100

    home knop en afsluiten knop toegevoegd

--- Plantenhotel/Dashboard.xaml.cs
+++ Plantenhotel/Dashboard.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
 
 namespace Plantenhotel
 {
@@ -15,6 +16,27 @@ namespace Plantenhotel
         private void Home_Loaded( object sender, RoutedEventArgs e )
         {
             MenuFrame.NavigationService.Navigate( new Overview() );
+        }
+
+        private void Afsluiten_Click( object sender, RoutedEventArgs e )
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Home_Click( object sender, RoutedEventArgs e )
+        {
+            DisplayWindow( new MainWindow() );
+        }
+
+        private void DisplayWindow( Window windowToShow )
+        {
+            for ( int i = 0; i < Application.Current.Windows.OfType<Window>().
+            Where( w => w.IsVisible ).Count(); i++ )
+            {
+                Window windowToHide = Application.Current.Windows[i];
+                windowToHide.Visibility = Visibility.Collapsed;
+            }
+            windowToShow.Visibility = Visibility.Visible;
         }
     }
 }
