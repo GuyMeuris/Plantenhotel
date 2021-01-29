1ae9fc09862b586059ed591f456312722c0781b2
Nigel <nigel.demaeght@gmail.com>
2021-01-29 09:39:17 +0100

    .

--- Plantenhotel/Klant.cs
+++ Plantenhotel/Klant.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
+using System.IO;
+using System.Text;
+
 namespace Plantenhotel
 {
     internal class Klant : Persoon
@@ -56,24 +59,7 @@ namespace Plantenhotel
                                             string wachtwoord) :
             base(achternaam, voornaam, geboortedatum, gsmnr, email, gebruikersnaam, wachtwoord)
         {
-            lijstKlanten.Add(this);
-            System.IO.FileStream klantGegevens;
-            byte[] gegevens = null;
-            gegevens = Encoding.ASCII.GetBytes(achternaam + ";" + voornaam + ";" + gsmnr + ";" + geboortedatum + ";" + email + ";" + Environment.NewLine);
-            klantGegevens = new FileStream(@"..\..\..\Tekstbestanden\gegevensKlant.txt", FileMode.Append);
-            klantGegevens.Write(gegevens, 0, gegevens.Length);
-            klantGegevens.Close();
-
-            System.IO.FileStream gebrww;
-            byte[] gww = null;
-
-            string encrGebruiker = AEScrypto.Encryptie(gebruikersnaam);
-            string encrWachtwoord = AEScrypto.Encryptie(wachtwoord);
-            gww = Encoding.ASCII.GetBytes(encrGebruiker + " " + encrWachtwoord + Environment.NewLine);
-            gebrww = new FileStream(@"..\..\..\Tekstbestanden\GebruikersnaamWachtwoord.txt", FileMode.Append);
-            gebrww.Write(gww, 0, gww.Length);
             
-            gebrww.Close();
         }
 
 
